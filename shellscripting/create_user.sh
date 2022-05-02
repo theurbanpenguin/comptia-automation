17401c6c02eaad4e6e52671660ff942e6bde2c99
Andrew Mallett <andrewmallett90@gmail.com>
2022-05-02 12:22:07 +0100

    create_user space

--- shellscripting/create_user.sh
+++ shellscripting/create_user.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 read -p "Enter a username to create: " NEWUSER
-echo ""
 grep -q $NEWUSER /etc/passwd && exit 2
 PASSWORD=$(./pwgen.sh)
 sudo useradd -m $NEWUSER -p $PASSWORD
+echo ""
 getent passwd $NEWUSER
