fc9a546a50c9666a7b2f9525f90834fb333ce907
Andrew Mallett <andrewmallett90@gmail.com>
2022-05-02 13:08:14 +0100

    test args

--- shellscripting/pwgen.sh
+++ shellscripting/pwgen.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 PWD_OK="false"
-
 while [ "$PWD_OK" != "true" ] ; do
-  read -sp "Enter password: "
-  echo ""
+  if [[ $# -ne 1  ]] ; then
+    read -sp "Enter password: "
+    echo ""
+  else
+    REPLY=$1
+fi
   PWD_LEN=$(echo -n "$REPLY" | wc -m)
   PASSWORD=$(openssl passwd -6 "$REPLY")
   if [ "$PWD_LEN" -gt 6 ]; then
