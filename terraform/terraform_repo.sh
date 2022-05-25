b1486583a9b3d29aeb88be7aba503e9e51eb8406
Andrew Mallett <andrewmallett90@gmail.com>
2022-05-25 10:44:43 +0100

    terraform repo

---
+++ terraform/terraform_repo.sh
@@ -0,0 +1,2 @@
+wget -qO - terraform.gpg https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/terraform-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/terraform-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/terraform.list
